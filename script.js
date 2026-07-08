a82b6205a883917dd401a19ea90dd73190b8cc20
ARTHUR ROSSI FRIEDRICH <arthur_r_friedrich@estudante.sesisenai.org.br>
2026-07-08 10:11:20 -0300

    five commit

---
+++ script.js
@@ -0,0 +1,35 @@
+function trocarText() {
+    document.getElementById("confrontos").textContent = ("Quartas de Final");
+}
+
+function trocarText() {
+    document.getElementById("pais1").textContent = "Brasil";
+    document.getElementById("pais2").textContent = "Noruega";
+
+    document.getElementById("bandeirinha1").src = "img/brasil.jpg";
+    document.getElementById("bandeirinha2").src = "img/noruega.jpg";
+}
+
+function trocarText2() {
+    document.getElementById("pais1").textContent = "Brasil";
+    document.getElementById("pais2").textContent = "Inglaterra";
+
+    document.getElementById("bandeirinha1").src = "img/brasil.jpg";
+    document.getElementById("bandeirinha2").src = "img/inglaterra.svg";
+}
+
+function trocarText3() {
+    document.getElementById("pais1").textContent = "Brasil";
+    document.getElementById("pais2").textContent = "Argentina";
+
+    document.getElementById("bandeirinha1").src = "img/brasil.jpg";
+    document.getElementById("bandeirinha2").src = "img/argentina.webp";
+}
+
+function trocarText4() {
+    document.getElementById("pais1").textContent = "Brasil";
+    document.getElementById("pais2").textContent = "França";
+
+    document.getElementById("bandeirinha1").src = "img/brasil.jpg";
+    document.getElementById("bandeirinha2").src = "img/frança.svg";
+}
